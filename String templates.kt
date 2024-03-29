554feb3e2230922c3324aee207aa4746b45febd6
rougeuser <160385462+rougeuser@users.noreply.github.com>
2024-03-29 20:56:24 +0530

    Create String templates.kt

---
+++ String templates.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val discountPercentage: Int = 0
+    val offer: String = ""
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    
+    println(offer)
+}
