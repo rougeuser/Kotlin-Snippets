556dd7385a069853fed3aa2c8bc42f8f29e4f2d9
rougeuser <160385462+rougeuser@users.noreply.github.com>
2024-03-31 20:46:45 +0530

    Create Column.kt

---
+++ Column.kt
@@ -0,0 +1,23 @@
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ElevatedButton
+// ...
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { /* TODO */ }
+            ) {
+                Text("Show more")
+            }
+        }
+    }
+}
