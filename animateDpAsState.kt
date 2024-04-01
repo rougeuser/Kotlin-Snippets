ca08c11a078307de551fbace8e7e8dbff00cfb89
rougeuser <160385462+rougeuser@users.noreply.github.com>
2024-04-01 21:03:34 +0530

    Create animateDpAsState.kt

---
+++ animateDpAsState.kt
@@ -0,0 +1,31 @@
+import androidx.compose.animation.core.animateDpAsState
+
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp
+    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+
+        }
+    }
+}
